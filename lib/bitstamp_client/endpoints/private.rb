16a37017f0e0aa98d50bc6d3e37ac3642f8ef24d
Gašper Košmrlj <gasperk@abelium.eu>
2017-04-02 10:42:25 +0200

    fix private endpoints and method names

--- lib/bitstamp_client/endpoints/private.rb
+++ lib/bitstamp_client/endpoints/private.rb
@@ -13,12 +13,12 @@ module BitstampClient
           :user_transactions => [:user_transactions],
           :open_orders => [:open_orders, params: [:pair]],  # pair = all, return orders of all pairs
           :cancel_order => [:cancel_order, params: [:id]],
-          :buy_limit_order => [:buy, params: [:pair, :amount, :price, :limit_price]],
-          :buy_market_order => ['buy/market', params: [:pair, :amount]],
-          :sell_limit_order => [:sell, params: [:pair, :amount, :price, :limit_price]],
-          :sell_market_order => ['sell/market', params: [:pair, :amount]],
-          :ripple_withdrawal => [:xrp_withdrawal, params: [:amount, :address]],
-          :ripple_address => [:xrp_address]
+          :buy => [:buy_limit_order, params: [:pair, :amount, :price, :limit_price]],
+          'buy/market' => [:buy_market_order, params: [:pair, :amount]],
+          :sell => [:sell_limit_order, params: [:pair, :amount, :price, :limit_price]],
+          'sell/market' => [:sell_market_order, params: [:pair, :amount]],
+          :xrp_withdrawal => [:ripple_withdrawal, params: [:amount, :address]],
+          :xrp_address => [:ripple_address]
         }
       end
 
@@ -27,7 +27,7 @@ module BitstampClient
           :withdrawal_requests => [:withdrawal_requests],
           :bitcoin_withdrawal => [:bitcoin_withdrawal, params: [:amount, :address]],
           :bitcoin_deposit_address => [:bitcoin_deposit_address],
-          :unconfirmed_bitcoint_deposits => [:unconfirmed_btc]
+          :unconfirmed_btc => [:unconfirmed_bitcoint_deposits]
         }
       end
     end
